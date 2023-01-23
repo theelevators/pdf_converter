997bf343e1754d4402264c77f430a0192b6d6d38
Tom Vazquez <elevatorstest@gmail.com>
2023-01-23 12:53:21 -0700

    Added padding to confirmation component

--- frontend/pdf-img-convrt/src/scenes/confirmation/index.jsx
+++ frontend/pdf-img-convrt/src/scenes/confirmation/index.jsx
@@ -11,7 +11,7 @@ import { Box } from "@mui/material";
 const ConfirmationPage = () => {
     
     return (
-        <Box>
+        <Box paddingTop="5rem">
         <Header
           title="Thank you for your submission!"
           subtitle="An email has been sent with a copy of the images."
